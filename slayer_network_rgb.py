2ce06d1bf493059623fbfb0fe755fc8c52ceba66
Johan Byttner <jbyttner@student.42.fr>
2019-06-02 13:56:12 +0200

    Add copyright preambles for thesis code

--- slayer_network_rgb.py
+++ slayer_network_rgb.py
@@ -1,3 +1,22 @@
+# Original work Copyright (c) 2019 Christoph Hofer
+# Modified work Copyright (c) 2019 Wolf Byttner
+# 
+# This file is part of the code implementing the thesis
+# "Classifying RGB Images with multi-colour Persistent Homology".
+# 
+#     This file is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Lesser General Public License as published
+#     by the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This file is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public License
+#     along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
 import torch
 import torch.nn as nn
 import numpy as np
